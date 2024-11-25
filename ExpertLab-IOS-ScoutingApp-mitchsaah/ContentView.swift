ffd831bd04146a105d85ad6947ffce61aa8821f6
mitchsaah <71660816+mitchsaah@users.noreply.github.com>
2024-11-25 22:42:15 +0100

    Put the function outside of button to call it up and saved positions

--- ExpertLab-IOS-ScoutingApp-mitchsaah/ContentView.swift
+++ ExpertLab-IOS-ScoutingApp-mitchsaah/ContentView.swift
@@ -9,42 +9,42 @@ struct ContentView: View {
                 .padding()
             
             // Button to write data TO Firestore
-                    Button(action: {
-                        // Function to write to Firestore
-                        writeToFirestore()
-                        func writeToFirestore() {
-                            let db = Firestore.firestore()
-                            let testDoc = db.collection("testCollection").document("testFirebase")
-
-                            // Writing test data
-                            testDoc.setData(["testField": "Hello, this is a test!"]) { error in
-                                if let error = error {
-                                    print("Error writing to Firestore: \(error.localizedDescription)")
-                                } else {
-                                    print("Data successfully written to Firestore!")
-                                }
-                            }
-                        }
-                    }) {
-                        Text("Write to Firestore")
-                            .padding()
-                            .background(Color.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(8)
-                    }
-
-                    // Button to read data FROM Firestore
-                    Button(action: {
-                        // Read function will go here
-                    }) {
-                        Text("Read from Firestore")
-                            .padding()
-                            .background(Color.green)
-                            .foregroundColor(.white)
-                            .cornerRadius(8)
-                    }
+            Button(action: {
+                writeToFirestore()
+            }) {
+                Text("Write to Firestore")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
                 }
+
+            // Button to read data FROM Firestore
+            Button(action: {
+            }) {
+                Text("Read from Firestore")
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+        }
         .padding()
+    }
+    
+    // Function to write to Firestore
+    func writeToFirestore() {
+        let db = Firestore.firestore()
+        let testDoc = db.collection("testCollection").document("testFirebase")
+
+        // Writing test data
+        testDoc.setData(["testField": "Hello, this is a test!"]) { error in
+            if let error = error {
+                print("Error writing to Firestore: \(error.localizedDescription)")
+            } else {
+                print("Data successfully written to Firestore!")
+            }
+        }
     }
 }
 
