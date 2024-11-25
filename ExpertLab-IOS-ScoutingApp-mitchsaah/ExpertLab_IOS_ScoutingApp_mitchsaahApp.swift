9dde370db5c2553d07311e7cdae2969c99549d01
mitchsaah <71660816+mitchsaah@users.noreply.github.com>
2024-11-25 15:12:14 +0100

    Initial Commit

---
+++ ExpertLab-IOS-ScoutingApp-mitchsaah/ExpertLab_IOS_ScoutingApp_mitchsaahApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpertLab_IOS_ScoutingApp_mitchsaahApp.swift
+//  ExpertLab-IOS-ScoutingApp-mitchsaah
+//
+//  Created by Mitch Saah on 25/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpertLab_IOS_ScoutingApp_mitchsaahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
