d1e80fc7db7b764a238a4ad040b8b6bdbd3359a8
mitchsaah <71660816+mitchsaah@users.noreply.github.com>
2024-11-25 21:34:34 +0100

    Added buttons for Firestore write and read functionality

--- ExpertLab-IOS-ScoutingApp-mitchsaah/ContentView.swift
+++ ExpertLab-IOS-ScoutingApp-mitchsaah/ContentView.swift
@@ -1,20 +1,35 @@
-//
-//  ContentView.swift
-//  ExpertLab-IOS-ScoutingApp-mitchsaah
-//
-//  Created by Mitch Saah on 25/11/2024.
-//
-
 import SwiftUI
+import FirebaseFirestore
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+        VStack(spacing: 20) {
+            Text("Firestore test")
+                .font(.largeTitle)
+                .padding()
+            
+            // Button to write data TO Firestore
+                    Button(action: {
+                        // Write function will go here
+                    }) {
+                        Text("Write to Firestore")
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
+                    }
+
+                    // Button to read data FROM Firestore
+                    Button(action: {
+                        // Read function will go here
+                    }) {
+                        Text("Read from Firestore")
+                            .padding()
+                            .background(Color.green)
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
+                    }
+                }
         .padding()
     }
 }
