f3ad727024f210e9a8153845778fc029ecb20d08
mitchsaah <71660816+mitchsaah@users.noreply.github.com>
2024-11-26 13:17:49 +0100

    Created an AuthenticationView with some basic structure

---
+++ ExpertLab-IOS-ScoutingApp-mitchsaah/Views/AuthenticationView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct AuthenticationView: View {
+    var body: some View {
+        VStack {
+            Text("Sign In")
+                .font(.largeTitle)
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    AuthenticationView()
+}
