09cd6e5c8cf594d430acac507939971eab505d56
mitchsaah <71660816+mitchsaah@users.noreply.github.com>
2024-11-26 11:46:10 +0100

    Added basic structure for the RoleSelectionView

---
+++ ExpertLab-IOS-ScoutingApp-mitchsaah/Views/RoleSelectionView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct RoleSelectionView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Select Your Role")
+                .font(.largeTitle)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    RoleSelectionView()
+}
