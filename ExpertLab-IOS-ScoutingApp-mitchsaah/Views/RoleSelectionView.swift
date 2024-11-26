2313e007683ca5cf52e85932032b7bace8292e2b
mitchsaah <71660816+mitchsaah@users.noreply.github.com>
2024-11-26 12:16:16 +0100

    Added buttons for roles (Scout & Coach)

--- ExpertLab-IOS-ScoutingApp-mitchsaah/Views/RoleSelectionView.swift
+++ ExpertLab-IOS-ScoutingApp-mitchsaah/Views/RoleSelectionView.swift
@@ -5,6 +5,23 @@ struct RoleSelectionView: View {
         VStack(spacing: 20) {
             Text("Select Your Role")
                 .font(.largeTitle)
+            // Scout button
+            Button("I am a Scout") {
+                print("Scout role selected")
+            }
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(8)
+
+            // Coach button
+            Button("I am a Coach") {
+                print("Coach role selected")
+            }
+            .padding()
+            .background(Color.red)
+            .foregroundColor(.white)
+            .cornerRadius(8)
         }
         .padding()
     }
