898f5aefe8a6a6d695a65f67ea5dac8f3f4a8d9f
mitchsaah <71660816+mitchsaah@users.noreply.github.com>
2024-12-05 15:09:15 +0100

    Added additional properties to the Player model - Updated the Player model to include new properties: email and createdBy - Made the id a String instead of UUID to align with the Firebase’s document ID usage - Added an initializer to simplify object creation with default values

--- ExpertLab-IOS-ScoutingApp-mitchsaah/Models/Player.swift
+++ ExpertLab-IOS-ScoutingApp-mitchsaah/Models/Player.swift
@@ -1,11 +1,35 @@
 import Foundation
 
 struct Player: Identifiable, Codable {
-    let id: UUID // ID of player
+    let id: String // ID of player
     var name: String
     var age: Int
+    var email: String
     var country: String
     var position: String
     var playerRole: String
     var currentClub: String
+    var createdBy: String
+    
+    init(
+        id: String = UUID().uuidString,
+        name: String = "",
+        age: Int = 19,
+        email: String = "",
+        country: String = "",
+        position: String = "",
+        playerRole: String = "",
+        currentClub: String = "",
+        createdBy: String = ""
+    ) {
+        self.id = id
+        self.name = name
+        self.age = age
+        self.email = email
+        self.country = country
+        self.position = position
+        self.playerRole = playerRole
+        self.currentClub = currentClub
+        self.createdBy = createdBy
+    }
 }
